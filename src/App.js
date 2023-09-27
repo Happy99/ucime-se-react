dde54c62f275fd5ea39eac61eedf950ca675f28d
Petr Šťastný <petr.stastny99@gmail.com>
2023-09-27 17:45:06 +0200

    upravy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import Book from "./components/Book"
 
 
+const books = [
+    {
+      id: 1,
+      image: "https://www.knihydobrovsky.cz/thumbs/book-list/mod_eshop/produkty/h/harry-potter-a-kamen-mudrcu-9788000061917.jpg.webp",
+      title: "Harry Potter a Kámen mudrců"
+    },
+    {
+      id: 2,
+      image: "https://www.knihydobrovsky.cz/thumbs/book-list/mod_eshop/produkty/333420609/10.jpg.webp",
+      title: "Harry Potter a Tajemná komnata"
+    },
+    {
+      id: 3,
+      image: "https://www.knihydobrovsky.cz/thumbs/book-list/mod_eshop/produkty/h/harry-potter-a-vezen-z-azkabanu-9788000063393.jpg.webp",
+      title: "Harry Potter a Vězeň z Azkabanu"
+    },
+]
+
 const App = () => {
     return (
-        <>
-            <h1>Prvni komponenta</h1>
-            <h2>{2+2}</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-            Facere ducimus magni nesciunt non autem natus corporis beatae 
-            architecto veniam similique, eos aliquid aperiam illum porro aut cum? Quam, aspernatur facere!
-            </p>
-
-            <Book />
-        </>
+        <div className="main-wrapper">
+            <h1>Favorite books</h1>
+            <div className="books-wrapper">
+                {books.map(book => (
+                    <Book key={book.id} image={book.image} title={book.title} />
+                ))}
+            </div>
+        </div>
     )
 }
 
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,13 @@
 import "./Book.css"
+import OrderButton from "./OrderButton"
 
-const Book = () => {
+const Book = (props) => {
     return (
         <div className="book">
-            <h2 className="main-heading">Název knihy: </h2>
+            <h2 className="main-heading">{props.title}</h2>
+            <img src={props.image} className="main-image" alt={props.title} />
+            <p className="main-annotation">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <OrderButton />
         </div>
     )
 }
